92795e043b49ffb2c46ca5ff8ad0b2b98043ab3d
rramankutty <rramankutty@lendingclub.com>
2017-06-27 11:16:24 -0700

    Add descriptions for each flag

--- kilo.c
+++ kilo.c
@@ -7,18 +7,20 @@
 struct termios orig_termios;
 
 void disableRawMode() {
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); // set term attributes to ones saved in &orig_termios when program exits
 }
 void enableRawMode() {
 	tcgetattr(STDIN_FILENO, &orig_termios); //read a terminal's attributes into struct
-	atexit(disableRawMode);
+	atexit(disableRawMode); //atexit in stdlib.h
 
 	struct termios raw = orig_termios;
-	raw.c_iflag &= ~(ICRNL| IXON); 
-	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);//disable carriage-return/nl ctrl-m ctrl-s ctrl-q -- stops data from being transmitted to the terminal. 
+	// ICRNL -  Carriage Return and New Line | IXON - Software Control Flow - pauses data transmission to terminal
+    // BRKINT - Like pressing Ctrl-C? | INPCK - parity checking. doesn't seem to apply to modern term.| ISTRIP - 8th bit is stripped. Already turned off
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 	raw.c_oflag &= ~(OPOST); //output post processing - terminal reads \n as \r\n so now we're chillin.
 	raw.c_cflag |= (CS8); // bitmask that sets character size to 8 bits per byte because fuck unicode
 	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); //negate echo | canonical mode | ctrl-v (term waits for you to type annother character) | Ctrl-c ctrl-y ctrl-z 
+
 	//set the terminal attributes - TCSAFLUSH == makes change until all queued output has been written and disregards queued input
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); 
 }
